6336b214437001799288e6ec02a1f4fcdec58fda
liran <liran.meir@clarizen.com>
2019-09-23 10:47:02 +0300

    Updates

---
+++ precache-manifest.45639a46af4a81d9eda9a7ae7d1e1516.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82cdd729041012dd7aa29788da3d38a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6e26cd3fd5d91eaa855",
+    "url": "/static/css/main.fe397867.chunk.css"
+  },
+  {
+    "revision": "356ebad6a0932407a01c",
+    "url": "/static/js/2.d73da99e.chunk.js"
+  },
+  {
+    "revision": "e6e26cd3fd5d91eaa855",
+    "url": "/static/js/main.0c485132.chunk.js"
+  },
+  {
+    "revision": "aedf08bfd231bc1f44d7",
+    "url": "/static/js/runtime~main.7677ad42.js"
+  }
+]);
